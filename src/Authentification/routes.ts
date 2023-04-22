03fd7b43988a644d30210e9122f8c053c791091b
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2023-04-22 10:33:19 +0200

    [ADD] login

--- src/Authentification/routes.ts
+++ src/Authentification/routes.ts
@@ -14,7 +14,8 @@ import { Crypt } from "./crypt";
 import { Token } from "./token";
 
 const router = express.Router();
-const PATH = "/api/v1/register";
+const PATH_REGISTER = "/api/v1/register";
+const PATH_LOGIN = "/api/v1/login";
 
 export async function check_email_exist(user: IUser) {
     const res = await UserModel.findOne({
@@ -25,8 +26,18 @@ export async function check_email_exist(user: IUser) {
     }
 }
 
+export async function find_user_by_email(user: IUser) {
+    const res = await UserModel.findOne({
+        email: user.email,
+    }).clone();
+    if (res == null) {
+        throw { error: { msg: "User does not exist" } };
+    }
+    return res;
+}
+
 router.post(
-    `${PATH}`,
+    `${PATH_REGISTER}`,
     body("username").exists().isString().isLength({ max: 12 }),
     body("email").exists().isString().isEmail(),
     body("password").isString().isLength({ min: 6, max: 50 }),
@@ -42,6 +53,34 @@ router.post(
         try {
             await check_email_exist(user);
             await user.save();
+            return res.status(utils.STATUS.Created).send({
+                refresh_token: await Token.generate_refresh_token(user),
+                access_token: await Token.generate_access_token(user),
+                expires_in: "1800s",
+                token_type: "Bearer",
+            });
+        } catch (err) {
+            return res.status(utils.STATUS.InternError).send(err);
+        }
+    }
+);
+
+router.post(
+    `${PATH_LOGIN}`,
+    body("email").exists().isString().isEmail(),
+    body("password").isString().isLength({ min: 6, max: 50 }),
+    async (req: express.Request, res: express.Response) => {
+        if (utils.VALIDATION.isError(req, res)) {
+            return;
+        }
+        let user = new UserModel({
+            email: req.body.email,
+            password: req.body.password,
+        });
+        try {
+            user = await find_user_by_email(user);
+            if (await Crypt.compare_password(req.body.password, user.password))
+                throw { error: { msg: "Bad password" } };
             return res.status(utils.STATUS.Created).send({
                 refresh_token: await Token.generate_refresh_token(user),
                 access_token: await Token.generate_access_token(user),
--- src/Authentification/token.ts
+++ src/Authentification/token.ts
@@ -21,40 +21,22 @@ export const Token = {
             }
         );
     },
-    // decode_token: async (token: string, key: string) => {
-    //     if (token == undefined) {
-    //         throw new GraphQLError(CustomErrorMessage.NO_AUTHORIZATION, {
-    //             extensions: {
-    //                 status: StatusCodes.FORBIDDEN,
-    //                 error: ReasonPhrases.FORBIDDEN,
-    //                 field: "Authorization",
-    //             },
-    //         });
-    //     }
-    //     const token_section = token.split(" ");
-    //     if (token_section.length != 2 || token_section[0] != "Bearer") {
-    //         throw new GraphQLError(CustomErrorMessage.INVALID_TOKEN, {
-    //             extensions: {
-    //                 status: StatusCodes.FORBIDDEN,
-    //                 error: ReasonPhrases.FORBIDDEN,
-    //                 field: "Authorization",
-    //             },
-    //         });
-    //     } else {
-    //         if (jsonwebtoken.verify(token_section[1], key as string)) {
-    //             const decodedToken = jsonwebtoken.decode(token_section[1], {
-    //                 complete: true,
-    //             });
-    //             return decodedToken;
-    //         } else {
-    //             throw new GraphQLError(CustomErrorMessage.INVALID_TOKEN, {
-    //                 extensions: {
-    //                     status: StatusCodes.FORBIDDEN,
-    //                     error: ReasonPhrases.FORBIDDEN,
-    //                     field: "Authorization",
-    //                 },
-    //             });
-    //         }
-    //     }
-    // },
+    decode_token: async (token: string, key: string) => {
+        if (token == undefined) {
+            throw { error: { msg: "No authorization" } };
+        }
+        const token_section = token.split(" ");
+        if (token_section.length != 2 || token_section[0] != "Bearer") {
+            throw { error: { msg: "Your Bearer token is invalid" } };
+        } else {
+            if (jsonwebtoken.verify(token_section[1], key as string)) {
+                const decodedToken = jsonwebtoken.decode(token_section[1], {
+                    complete: true,
+                });
+                return decodedToken;
+            } else {
+                throw { error: { msg: "Your Bearer token is invalid" } };
+            }
+        }
+    },
 };
