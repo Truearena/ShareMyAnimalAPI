2f90648776dc9555564534ecf3833577f8bdae8f
Karim YAZAMI <karim.yazami@epitech.eu>
2023-04-25 14:15:25 +0200

    add service

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,15 @@ import routes from "./routes";
 import dotenv from "dotenv";
 
 const PORT = 80;
-const MONGO_URI = "mongodb://localhost:27018/shareMyAnimal";
+const MONGO_URI = "mongodb://127.0.0.1:27018/shareMyAnimal";
 
 dotenv.config();
 
+console.log(MONGO_URI)
 mongoose
     .connect(MONGO_URI)
     .then((res) => {
+        console.log('OK CONNECTED mongoose')
         const app = express();
         app.use(json());
         if (Array.isArray(routes)) {
